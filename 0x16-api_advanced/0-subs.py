4b94752c8c317666e593e0c1f0d02504adb965aa
Queendarlin <queendarlinnnamani@gmail.com>
2024-05-07 15:49:11 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for number_of_subscribers function"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {'User-Agent': 'myAlxScript/0.0.1'}
+
+    response = requests.get(url, headers=header, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
